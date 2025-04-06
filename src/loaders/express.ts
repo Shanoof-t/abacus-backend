474966a7549fc33b1dc35f8de9cba615a7cc19a5
zedworkspace <shanoof.001@gmail.com>
2025-04-07 01:09:03 +0530

    feat:add live front end origins for cors sharing

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -7,7 +7,16 @@ import { LoaderParams } from "../types/loader-types";
 
 export default async ({ app, express }: LoaderParams) => {
   app.use(express.json());
-  const allowedOrigins = ["http://localhost:3000", "http://127.0.0.1:3000"];
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://abacuss.online",
+    "http://www.abacuss.online",
+    "https://abacuss.online",
+    "https://www.abacuss.online",
+    "http://3.110.46.217:8080",
+    "https://3.110.46.217:8080",
+  ];
 
   app.use(
     cors<Request>({

34848aa377483727a21c4f3f2943ce93c586d210
zedworkspace <shanoof.001@gmail.com>
2025-05-26 15:58:51 +0530

    feat(bank):implemented bank integration

--- src/utils/format-cron-expression.ts
+++ src/utils/format-cron-expression.ts
@@ -12,12 +12,12 @@ function formatCronExpression({ nextDate }: { nextDate: Date | string }) {
   console.log(`Scheduled time: ${scheduledTime}`);
   console.log(`Actual expression: ${minute} ${hour} ${day} ${month} *`);
 
-  const currMin = format(new Date(), "m");
-  const currHou = format(new Date(), "H");
-  const parsed = Number(currMin) + 2;
-  const mock = `${parsed.toString()} ${currHou} 29 3 *`;
-  console.log("Mock expression:", mock);
-  return mock;
-  // return `${minute} ${hour} ${day} ${month} *`;
+  // const currMin = format(new Date(), "m");
+  // const currHou = format(new Date(), "H");
+  // const parsed = Number(currMin) + 2;
+  // const mock = `${parsed.toString()} ${currHou} 29 3 *`;
+  // console.log("Mock expression:", mock);
+  // return mock;
+  return `${minute} ${hour} ${day} ${month} *`;
 }
 export default formatCronExpression;

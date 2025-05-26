da3297a84cf1cf16a38912e2ccad825120e6f226
zedworkspace <shanoof.001@gmail.com>
2025-05-26 21:57:43 +0530

    change:change cors

--- src/sockets/index.ts
+++ src/sockets/index.ts
@@ -12,6 +12,7 @@ function init(server: http.Server) {
     cors: {
       origin: "https://abacuss.online",
       credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     },
   });
 
